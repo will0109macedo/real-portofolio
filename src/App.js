e5feff63288377e8db6b16b10d4e32be645e8e12
Will <will.macedo@hotmail.com>
2022-01-25 23:21:41 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Intro from './components/intro/Intro';
+
+function App() {
+  return (
+    <div>
+    <h1><Intro /></h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/intro/Intro.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './intro.css'
+
+const Intro = () => {
+  return <div className="intro">
+    <div className="intro-left">Left</div>
+    <div className="intro-right">Right</div>
+  </div>;
+};
+
+export default Intro;
